2aa14310f5a7d1d27a0eef439d44091ebb717248
aruna29 <tsydenova-01@mail.ru>
2025-02-06 17:26:17 +0900

    исправляет регистрационную форму и логин

--- src/public/handle_login.php
+++ src/public/handle_login.php
@@ -1,39 +1,23 @@
 <?php
 
-$errors = [];
-function ValidateLogin($username)
-    {
-        if (strlen($username) < 4) {
-            return 'Логин должен иметь больше 4 символов';
-        } elseif (filter_var($username, FILTER_VALIDATE_EMAIL) === false) {
-            return 'Логин некорректный';
-        }
+function Validate(array $data): array|null
+{
+    $errors = [];
+    if (empty($data['u'])) {
+        $errors['username'] = 'Логин не заполнен';
     }
-function ValidatePassword($password) {
-         if (strlen($password) < 4) {
-         return 'Пароль должен иметь больше 4 символов';
-        }
+    if (empty($data['p'])) {
+        $errors['password'] = 'пароль не заполнен';
     }
+    return $errors;
+}
 
-    if (isset($_POST['u'])) {
-        $username = $_POST['u'];
-        if (ValidateLogin($username)!== null) {
-            $errors ['username']= ValidateLogin($username);
-        }
-    } else {
-            $errors['username'] = 'login должен быть заполнен';
-    }
 
-    if (isset($_POST['p']) ) {
-        $password = $_POST['p'];
-        if (validatePassword($password)!== null) {
-            $errors['password'] = ValidatePassword($password);
-        }
-    } else {
-    $errors['password'] = 'пароль должен быть заполнен';
-    }
+$errors = Validate($_POST);
 
 if (empty($errors)) {
+    $username = $_POST['u'];
+    $password = $_POST['p'];
     $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=mydb", 'user', 'pass');
     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
     $stmt->execute(['email' => $username]);
--- src/public/index.php
+++ src/public/index.php
@@ -1,76 +1,62 @@
 <?php
 
-$errors = [];
+function Validate(array $data): array|null
+{
+    $errors = [];
 
-
-function ValidateName($name) {
-
-    if (strlen($name) < 1) {
-    return "Имя должно быть больше 1";
-    }   elseif (is_numeric($name)) {
-    return 'Имя не должно содержать цифры';
-    }
-}
-
-if (isset($_POST['name'])) {
-        $name = $_POST['name'];
-        if (ValidateName($name)!== null) {
-            $errors ['name']= ValidateName($name);
+    if (isset($data['name'])) {
+        $name = $data['name'];
+        if (strlen($name) < 2) {
+            $errors['name'] = 'Имя должно быть больше 2';
+        } elseif (is_numeric($name)) {
+            $errors['name'] = 'Имя не должно содержать цифры';
         }
     } else {
         $errors['name'] = 'Имя должно быть заполнено';
     }
 
-
-function ValidateEmail($email) {
-
-    if (strlen($email) < 3) {
-        return 'email должен быть больше 3 символов';
-    } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-        return 'email некорректный';
-    }  else  {
-        $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=mydb", 'user', 'pass');
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-        $stmt->execute(['email' => $email]);
-        $identEmail = $stmt->fetch();
-        if ($identEmail > 0) {
-            return 'email уже существует';
+    if (isset($data['email'])) {
+        $email = $data['email'];
+        if (strlen($email) < 3) {
+            $errors['email'] = 'email должен быть больше 3 символов';
+        } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $errors['email'] = 'email некорректный';
+        } else {
+            $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=mydb", 'user', 'pass');
+            $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+            $stmt->execute(['email' => $email]);
+            $identEmail = $stmt->fetch();
+            if ($identEmail > 0) {
+                $errors['email'] = 'email уже существует';
+            }
         }
-    }
-}
-
-
-if (isset($_POST['email'])) {
-    $email = $_POST['email'];
-    if (ValidateEmail($email)!== null) {
-        $errors ['email']= ValidateEmail($email);
-    }
-} else {
+    } else {
         $errors['email'] = 'email должен быть заполнен';
     }
 
-
-function ValidatePassword($password)  {
-    if (strlen($password) < 4) {
-        return 'пароль должен быть больше 4 символов';
+    if (isset($data['psw'])) {
+        $password = $data['psw'];
+        if (strlen($password) < 4) {
+            $errors['password'] = 'пароль должен быть больше 4 символов';
+        }
+        $passwordRep = $data['psw-repeat'];
+        if ($password !== $passwordRep) {
+            $errors['passwordRep'] = 'пароли не совпали';
+        }
+    } else {
+        $errors['password'] = 'пароль должен быть заполнен';
     }
+
+    return $errors;
 }
 
-
-if (isset($_POST['psw']) && isset($_POST['psw-repeat']) ) {
-    $password = $_POST['psw'];
-    $passwordRep = $_POST['psw-repeat'];
-    if (validatePassword($password)!== null) {
-        $errors['password'] = ValidatePassword($password);
-    } elseif ($password !== $passwordRep) {
-        $errors['passwordRep'] = 'пароли не совпали';
-    }
-} else {
-    $errors['password'] = 'пароль должен быть заполнен';
-}
-
+$errors = Validate($_POST);
 
 if (empty($errors)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['psw'];
+    $passwordRep = $_POST['psw-repeat'];
     $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=mydb", 'user', 'pass');
 
     $password = password_hash($password, PASSWORD_DEFAULT);
